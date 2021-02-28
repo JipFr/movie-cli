c5eaaceaa766832622330180e6fc992d8604dcb3
Jip Fr <jipfrijlink@gmail.com>
2021-02-28 16:15:34 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,155 @@
+// Imports
+import yargs from "yargs";
+import fetch from "node-fetch";
+import Fuse from "fuse.js";
+import * as json5 from "json5";
+import getVideoUrl from "./lookmovie";
+import { exec } from "child_process";
+
+// Process args
+const [searchTerm, season, episode] = process.argv.filter(
+	(v) => !v.startsWith("/")
+);
+let commandIndex = 0;
+
+async function main() {
+	if (season && !episode) {
+		console.error("Missing episode");
+		return;
+	}
+
+	// Do search, merge TV and movie results
+	const movieSearchRes = await fetch(
+		`https://lookmovie.io/api/v1/movies/search/?q=${encodeURIComponent(
+			searchTerm
+		)}`
+	).then((d) => d.json());
+	const showSearchRes = await fetch(
+		`https://lookmovie.io/api/v1/shows/search/?q=${encodeURIComponent(
+			searchTerm
+		)}`
+	).then((d) => d.json());
+
+	// Map results to array
+	const results = [
+		...movieSearchRes.result.map((v) => ({ ...v, type: "movie" })),
+		...showSearchRes.result.map((v) => ({ ...v, type: "show" })),
+	];
+
+	// @ts-ignore
+	const fuse = new Fuse(results, {
+		threshold: 0.3,
+		distance: 200,
+		keys: ["title"],
+	});
+	const matchedResults = fuse
+		.search(searchTerm.toString())
+		.map((result) => result.item);
+
+	// Now findthe first item and do the thing
+	const toShow = matchedResults[0];
+
+	if (!toShow) {
+		console.error("Unable to find that... Sorry!");
+		return;
+	}
+
+	console.log(`Scraping the ${toShow.type} "${toShow.title}"`);
+
+	// ! Now we get the ID and stuff we need
+	const url = `https://lookmovie.io/${toShow.type}s/view/${toShow.slug}`;
+	const pageReq = await fetch(url).then((d) => d.text());
+
+	// Extract and parse JSON
+	let scriptJson =
+		"{" +
+		pageReq
+			.slice(pageReq.indexOf(`${toShow.type}_storage`))
+			.split("};")[0]
+			.split("= {")[1]
+			.trim() +
+		"}";
+	const data = json5.parse(scriptJson);
+
+	// Find the relevant id
+	let id = null;
+	if (toShow.type === "movie") {
+		id = data.id_movie;
+	} else if (toShow.type === "show") {
+		const episodeObj = data.seasons.find((v) => {
+			return v.season == season && v.episode == episode;
+		});
+		if (episodeObj) id = episodeObj.id_episode;
+	}
+
+	// Check ID
+	if (id === null) {
+		console.error(`Not found: S${season} E${episode}`);
+		return;
+	}
+
+	// Generate object to send over to scraper
+	let reqObj = null;
+	if (toShow.type === "show") {
+		reqObj = {
+			slug: toShow.slug,
+			episodeId: id,
+			type: "tv",
+		};
+	} else if (toShow.type === "movie") {
+		reqObj = {
+			slug: toShow.slug,
+			movieId: id,
+			type: "movie",
+		};
+	}
+
+	if (!reqObj) {
+		console.error("Invalid type");
+		return;
+	}
+
+	const videoUrl = await getVideoUrl(reqObj);
+	console.log(videoUrl);
+	// const pageUrl = `https://xenodochial-mestorf-a8f181.netlify.app/?https://hidden-inlet-27205.herokuapp.com/${videoUrl}`
+	// exec(`/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app=${pageUrl}`)
+	attemptOpen(videoUrl);
+}
+
+function attemptOpen(videoUrl: string) {
+	// Bit of a hack, but oh well.
+	const maxTimeout = 5e3;
+	const now = Date.now();
+
+	// Define shell commands to attempt
+	const commands = [
+		`vlc ${videoUrl}`,
+		`/Applications/VLC.app/Contents/MacOS/VLC ${videoUrl}`,
+		`open -a Safari ${videoUrl}`,
+		`open ${videoUrl}`,
+	];
+
+	// Find relevant one and execute it
+	const command = commands[commandIndex];
+	if(command) {
+		exec(command, (err, _stdout, stderr) => {
+			const diff = Date.now() - now;
+			if ((err || stderr) && diff < maxTimeout) {
+				// If it has failed within the timer, attempt next
+				commandIndex++;
+				console.log(
+					`Command ${commandIndex} failed after`,
+					diff,
+					"ms, trying next"
+				);
+				attemptOpen(videoUrl);
+			} else {
+				console.log("Ended");
+			}
+		});
+	} else {
+		console.error("Looks like everything failed, weird.")
+	}
+}
+
+main();
---
+++ src/lookmovie.ts
@@ -0,0 +1,101 @@
+// There's no error handling
+// I know, and I'm sorry.... Probbaly :D
+
+import fetch from 'node-fetch'
+
+// ! Types
+interface BaseConfig {
+	/** The website's slug. Formatted as `1839578-person-of-interest-2011` */
+	slug: string;
+	/** Type of request */
+	type: "tv" | "movie";
+}
+interface TvConfig extends BaseConfig {
+	/** Type of request */
+	type: "tv";
+	/** The episode ID for a TV show. Given in search and URL */
+	episodeId: string;
+}
+interface MovieConfig extends BaseConfig {
+	/** Type of request */
+	type: "movie";
+	/** Movie's id */
+	movieId: string;
+}
+type Config = MovieConfig | TvConfig;
+
+// ! Configuration
+const cfg = {
+	// base: "https://hidden-inlet-27205.herokuapp.com/https://lookmovie.io",
+	base: "https://lookmovie.io",
+};
+
+// This is different per episode (or movie if I ever add that)
+const personOfInterestConfig: Config = {
+	slug: "1839578-person-of-interest-2011", // From the URL... obviously
+	episodeId: "17359", // Shown in URL and episode list
+	type: "tv",
+};
+const xmenConfig: Config = {
+	slug: "x-men-apocalypse-2016", // From the URL... obviously
+	movieId: "14358",
+	type: "movie",
+};
+
+// ! Methods / functions
+/**
+ * Get URL / data for thing
+ * Right now this method only works with TV shows afaik
+ */
+async function getVideoUrl(config: Config): Promise<string> {
+	// Person of Interest:
+	// https://lookmovie.io/manifests/shows/json/mbQFYTR499c9vfDmAwOFrg/1613868343/17359/master.m3u8
+
+	// X-men: Apocalypse
+	// https://lookmovie.io/manifests/movies/json/14358/1613868860/j5_imzkY3WkNR21KByjv0g/master.m3u8
+
+	// Tv show episode URL generation
+	const securityToken = await getSecurityToken(config);
+	const now = Math.floor(Date.now() / 1e3);
+
+	let url: string | null = null;
+	if (config.type === "tv") {
+		url = `${cfg.base}/manifests/shows/json/${securityToken}/${now}/${config.episodeId}/master.m3u8`;
+	} else if (config.type === "movie") {
+		url = `${cfg.base}/manifests/movies/json/${config.movieId}/${now}/${securityToken}/master.m3u8`;
+	}
+
+	if (url) {
+		const videoOpts = await fetch(url).then((d) => d.json());
+
+		// Find video URL and return it (with a check for a full url if needed)
+		const videoUrl = videoOpts.auto || videoOpts["720"];
+		return videoUrl.startsWith("/") ? `${cfg.base}${videoUrl}` : videoUrl;
+	}
+
+	return "Invalid type.";
+}
+
+async function getSecurityToken(config: Config): Promise<string> {
+	if (config.type === "tv") {
+		// 'mbQFYTR499c9vfDmAwOFrg' // Retrieved from: https://lookmovie.io/api/v1/security/show-access?slug=1839578-person-of-interest-2011&token=&step=2
+		const url = `${cfg.base}/api/v1/security/show-access?slug=${config.slug}&token=&step=2`;
+		const data = await fetch(url).then((d) => d.json());
+
+		const token = data.data.accessToken;
+		return token;
+	} else if (config.type === "movie") {
+		// https://lookmovie.io/api/v1/security/movie-access?id_movie=14358&token=1&sk=&step=1
+		const url = `${cfg.base}/api/v1/security/movie-access?id_movie=${config.movieId}&token=1&sk=&step=1`;
+		const data = await fetch(url).then((d) => d.json());
+
+		const token = data.data.accessToken;
+		return token;
+	}
+	return "Invalid type provided in config";
+}
+
+// console.log(await getVideoUrl(personOfInterestConfig));
+// console.log(await getVideoUrl(xmenConfig));
+
+export default getVideoUrl;
